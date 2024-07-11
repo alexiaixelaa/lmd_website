7cbaf5b64f66fb6fa67645ca5dcd8312e3ec6c06
alexiaixelaa <alexiasimental12@gmail.com>
2024-07-11 00:18:38 -0500

    new changes to the components and assets file

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/lmd_logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className='nav-container'>
+        <img src={logo} width='100' height='100' alt='Loving Maggies Daycare logo' className='logo'/>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Programs</li>
+            <li>Virtual Tour</li>
+            <li>Schedule</li>
+            <li>Parents</li>
+            <li>Contact Us</li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default Navbar
