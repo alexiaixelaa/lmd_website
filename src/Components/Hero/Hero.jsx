859828855d9a500731e7a2a35292f9a34996b83e
alexiaixelaa <alexiasimental12@gmail.com>
2024-07-14 00:29:58 -0500

    small updates

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -15,8 +15,8 @@ const Hero = () => {
             <path d="M-16 199.662C-16 186.944 -11.1537 174.705 -2.44738 165.434L67.6612 90.7804C82.1569 75.345 104.804 70.7143 124.195 79.2208L248.491 133.747C261.358 139.391 276.007 139.361 288.851 133.664L475.811 50.735C492.176 43.4759 511.174 45.5207 525.619 56.0961L645.189 143.63C659.82 154.342 679.104 156.292 695.583 148.727L880.447 63.8676C894.931 57.2192 911.716 57.8731 925.638 65.6281L1035.31 126.719C1047.1 133.287 1061.05 134.809 1073.98 130.937L1304.23 62.0039C1322.11 56.6504 1341.49 61.7027 1354.48 75.1065L1441.91 165.321C1450.95 174.649 1456 187.129 1456 200.118V579C1456 606.614 1433.61 629 1406 629H34C6.38574 629 -16 606.614 -16 579V199.662Z" fill="#71BF45"/>
             </g>
             <defs>
-                <filter id="filter0_d_41_98" x="-66" y="0.440125" width="1572" height="682.56" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                <filter id="filter0_d_41_98" x="-66" y="0.440125" width="1572" height="682.56" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
                     <feOffset dy="4"/>
                     <feGaussianBlur stdDeviation="25"/>
                     <feComposite in2="hardAlpha" operator="out"/>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/lmd_logo.png'
