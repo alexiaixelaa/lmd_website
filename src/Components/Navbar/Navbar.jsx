3b49b31be9c260bd010064f522e5ed3b2189ff81
alexiaixelaa <alexiasimental12@gmail.com>
2024-07-15 21:22:32 -0500

    minor updates to the nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,13 +8,27 @@ const Navbar = () => {
     <nav className='nav-container'>
         <img src={logo} width='100' height='100' alt='Loving Maggies Daycare logo' className='logo'/>
         <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Programs</li>
-            <li>Virtual Tour</li>
-            <li>Schedule</li>
-            <li>Parents</li>
-            <li>Contact Us</li>
+            <li className='nav-link'>
+              <a href='#home'>Home</a>
+              </li>
+            <li className='nav-link'>
+              <a href='#about-us'>About Us</a>
+              </li>
+            <li className='nav-link'>
+              <a href='#programs'>Programs</a>
+              </li>
+            <li className='nav-link'>
+              <a href='#virtual-tour'>Virtual Tour</a>
+              </li>
+            <li className='nav-link'>
+              <a href='#schedule'>Schedule</a>
+              </li>
+            <li className='nav-link'>
+              <a href='#parents'>Parents</a>
+              </li>
+            <li className='nav-link'>
+              <a href='#contact-us'>Contact Us</a>
+              </li>
         </ul>
 
     </nav>
